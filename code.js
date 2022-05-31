fb4a8e810649caeb9f0a619d295d9034f92bc262
YoYoDough <yosuf818s@gmail.com>
2022-05-31 00:50:43 -0700

    Just added a grid

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ window.onload = function()
 {
     var htmlElements = "";
     for (var i = 0; i < 256; i++) {
-        htmlElements += '<div class="box">hi</div>';
+        htmlElements += '<div class="box"></div>';
     }
     var container = document.getElementById("container");
     container.innerHTML = htmlElements;
