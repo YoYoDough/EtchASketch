1076ee87cbf4666480e84bf8235027725b2940a4
YoYoDough <yosuf818s@gmail.com>
2022-06-01 03:09:48 -0700

    Just tried to make input for the grid size but it didnt work

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
-window.onload = function()
-{
-    var container = document.getElementById("container");
-    for (var i = 0; i < 256; i++) {
-        const box = document.createElement('div')
-        box.classList.add('box')
-        box.addEventListener('mouseover', function hover() {
-            box.style.background = 'black';
-      });
-        container.appendChild(box)
-    }
+
+var container = document.getElementById("container");
+var buttn = document.getElementById("enter");
+
+for (var i = 0; i < 256; i++) {
+    const box = document.createElement('div')
+    box.classList.add('box')
+    box.addEventListener('mouseover', function hover() {
+        box.style.background = 'black';
+  });
+    container.appendChild(box)
 }
