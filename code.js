9b3e9cca1e16e3a59e99fa9023eec589f218f316
YoYoDough <yosuf818s@gmail.com>
2022-06-01 00:28:44 -0700

    Just made the grid be ablt to change color when mouse is over a box

--- code.js
+++ code.js
@@ -1,14 +1,12 @@
 window.onload = function()
 {
-    var htmlElements = "";
-    for (var i = 0; i < 256; i++) {
-        htmlElements += '<div class="box"></div>';
-    }
     var container = document.getElementById("container");
-    container.innerHTML = htmlElements;
-    var boxx = document.getElementById('box')
-    container.addEventListener('mouseover', function()
-    {
-        // container.style.backgroundColor = "black";
-    });
+    for (var i = 0; i < 256; i++) {
+        const box = document.createElement('div')
+        box.classList.add('box')
+        box.addEventListener('mouseover', function hover() {
+            box.style.background = 'black';
+      });
+        container.appendChild(box)
+    }
 }
