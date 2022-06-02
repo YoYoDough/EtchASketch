5342ccc27c7130224a18b6655606ff7ea603ddd1
YoYoDough <yosuf818s@gmail.com>
2022-06-01 22:04:14 -0700

    Just added a an input that takes how many boxes the user wants to add

--- code.js
+++ code.js
@@ -1,12 +1,22 @@
 
 var container = document.getElementById("container");
 var buttn = document.getElementById("enter");
-
-for (var i = 0; i < 256; i++) {
-    const box = document.createElement('div')
-    box.classList.add('box')
-    box.addEventListener('mouseover', function hover() {
-        box.style.background = 'black';
-  });
-    container.appendChild(box)
+var input = document.getElementById('text');
+var number = input.value;
+buttn.addEventListener('click', function()
+{
+    var number = input.value;
+    var product = number * number;
+    grid(product);
+});
+function grid(size)
+{
+    for (var i = 0; i < size; i++) {
+        const box = document.createElement('div')
+        box.classList.add('box')
+        box.addEventListener('mouseover', function hover() {
+            box.style.background = 'black';
+    });
+        container.appendChild(box)
+    }
 }
